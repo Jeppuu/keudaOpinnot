9cfebf3da1e511d9cdb8f9098eb9fb714d61b533
jeppuu <jennamariakoskelo@gmail.com>
2021-03-24 15:33:45 +0200

    viikon 2 toiset tehtävät aloitettu

---
+++ Javascript/viikko2/tokatTehtävät.js
@@ -0,0 +1,8 @@
+function posi() {
+  var luku = document.getElementById('luku').value;
+  if( luku >= 0) {
+    console.log("luku on positiivinen");
+  } else {
+    console.log("luku on negatiivinen");
+  }
+}
