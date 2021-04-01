fbbe278be534c48850dda10023468604869219b7
jeppuu <jennamariaksoskelo@gmail.com>
2021-04-01 12:54:57 +0300

    .

--- Javascript/DOMharjoituksia/registrationForm/registration.js
+++ Javascript/DOMharjoituksia/registrationForm/registration.js
@@ -4,22 +4,29 @@
   ------------
   Add a label to each of the input fields: username, password, confirm password
 */
-document.getElementById('username')
-.insertAdjacentHTML('beforeBegin','<label for= "username">Username: </label>');
+document.getElementById('username').insertAdjacentHTML('beforeBegin','<label for= "username">Username: </label>');
 
-document.getElementById('password')
-.insertAdjacentHTML('beforeBegin','<label for= "password">Password: </label>');
+document.getElementById('password').insertAdjacentHTML('beforeBegin','<label for= "password">Password: </label>');
 
-document.getElementById('confirmPassword')
-.insertAdjacentHTML('beforeBegin','<label for= "confirmPassword">Confirm Password: </label>');
+document.getElementById('confirmPassword').insertAdjacentHTML('beforeBegin','<label for= "confirmPassword">Confirm Password: </label>');
 
 /*
   Exercise 02
   -----------
   Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
 */
+const checkInput = (event) => {
+  const errorElement = Array.from(event.target.parentNode.querySelectorAll('span'));
+  if(event.target.value === ''&& !errorElement.length) {
+    event.target.insertAdjacentHTML('afterEnd', 'span class="text-danger">Required</span>')
+  }
+  if (errorElement && event.target.value !== '') {
+    errorElement.forEach(elem => elem.remove());
+  }
+};
 
-
+document.getElementById('username')
+.addEventlistener('blur', checkInput);
 /*
   Exercise 03
   -----------
